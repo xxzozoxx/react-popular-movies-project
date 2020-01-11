a0032d0b66059ca7616bde5fe69b301cc674b18f
zozo <zahraa.jalaa99@gmail.com>
2020-01-11 16:05:38 +0300

    set up rootReducer

---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+import combineReducers from "redux";
+import { connectRouter } from "connected-react-router";
+import home from "./reducers/home";
+const createRootReducer = history =>
+  combineReducers({
+    router: connectRouter(history),
+    home
+  });
+  export default createRootReducer;
