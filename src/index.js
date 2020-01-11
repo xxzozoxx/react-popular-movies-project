8d54930b2e9b83a47553c4fdd6a8b0fdfd7c0ad1
zozo <zahraa.jalaa99@gmail.com>
2020-01-11 16:01:12 +0300

    set up root component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Main from "./Main";
+import {Provider} from "react-redux";
+import {ConnectedRouter} from "connected-react-router";
+import {Switch,Route} from "react-router-dom";
+import { configStore, history } from "./store";
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+const store = configStore(); 
+ReactDOM.render(
+<Provider store={store}>
+<ConnectedRouter history={history}>
+    <Switch>
+    <Route exact path="/" component={Main}/>
+    <Route path="/movies" render={() => <h1>Movie Page</h1>}/>
+    </Switch>
+</ConnectedRouter>
+</Provider> ,
+ document.getElementById("root"));
