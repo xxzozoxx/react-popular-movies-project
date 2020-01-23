e0edfacb7c1f2ee34a142ef95c574052f433a7e1
zozo <zahraa.jalaa99@gmail.com>
2020-01-24 01:20:07 +0300

    disable devtools in production

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {disableReactDevTools} from "./utils";
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,6 +9,11 @@ import configStore, { history } from "./store";
 import { ConnectedRouter } from "connected-react-router";
 import { APP_NAME } from "./constants";
 import { fetchConfigurations, fetchGenresList } from "./actions";
+
+if(process.env.NODE_ENV === "produvtion"){
+  disableReactDevTools();
+}
+
 const store = configStore(/* provide initial or preloaded state if any*/);
 store.dispatch(fetchConfigurations());
 store.dispatch(fetchGenresList());
@@ -20,3 +26,31 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+export function disableReactDevTools(){
+  function isFunction(obj){
+    return typeof obj == "function" || false;
+  }
+  function isArray(obj) {
+    return typeof obj.forEach == "function";
+  }
+
+  function isObject(obj) {
+    var type = typeof obj;
+    return type === "function" || (type === "object" && !!obj);
+  }
+  if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+    return;
+  }
+  
+  const NO_OP = () => {};
+  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
+    )
+      ? NO_OP
+      : isArray(window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop])
+      ? []
+      : null;
+  }
+}
