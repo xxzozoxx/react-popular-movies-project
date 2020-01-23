32aefa5138a80bf1465be7c5e38565a1c48fc51e
zozo <zahraa.jalaa99@gmail.com>
2020-01-24 01:14:50 +0300

    Deleted excess unused code

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -19,7 +19,7 @@ export const Movie = ({ movie }) => {
 
    return(
      <>
-     <OverView movie={movie} images={images} />
+     <OverView movie={movie} images={images} id="overview"/>
      <PageNavigator
           offsetElementTop={0}
           offsetContainerTop={0}
@@ -29,7 +29,7 @@ export const Movie = ({ movie }) => {
      </>
    )
   };
-const OverView = ({movie,images}) => {
+const OverView = ({movie,images,id}) => {
   const [prominentColor,setProminentColor] = useState(null);
 
   const original_language = find(languages,{
@@ -40,20 +40,23 @@ let backdropSize = "w1280";
 if(window.innerWidth < 1000) {
   backdropSize = "w780";
 }
-const updateColors = e => {
-  let color = Color.rgb(new ColorThief().getColor(e.target));
-  color = color.saturate(0.8);
-  if(color.isLight()) color = color.darken(0.5);
-  setProminentColor(color);
+
   
   useEffect(() => {
     if(!images.backdrop) return;
+
     const img = new Image();
+
     img.crossOrigin = "anonymous";
-    img.addEventListener("load",updateColors);
+    const onLoad = e => {
+      let color = Color.rgb(new ColorThief().getColor(e.target)).saturate(0.8);
+      if (color.isLight()) color = color.darken(0.5);
+      setProminentColor(color);
+    };
+    img.addEventListener("load",onLoad);
     img.src = images.backdrop.w300;
     return () => {
-      img.removeEventListener("load",updateColors);
+      img.removeEventListener("load",onLoad);
     };
   },[images]);
 
@@ -62,7 +65,7 @@ const updateColors = e => {
   style.backgroundImage = `url(${images.backdrop[backdropSize]})`;
   
   return(
-    <div className="movie-overview" style={style} id="overview">
+    <div className="movie-overview" style={style} id={id}>
       <div
         className={images.backdrop ? "has-backdrop" : ""}
         style={{
@@ -76,12 +79,14 @@ const updateColors = e => {
                 <Row>
                   <Col xs={12} md={5}>
                     <div className="my-2 my-md-5">
-                      <MoviePoster poster={images.poster.w500}></MoviePoster>
+                      <div className="movie-poster my-2 mx-auto m-md-2">
+                        <img src={images.poster.w500} alt="Movie Poster" />
+                      </div>
                     </div>
                   </Col>
                   <Col>
-                    <div className="movie-body-overview my-2 my-md-5">
-                      <div className="mb-5">
+                    <div className="my-2 my-md-5">
+                      <section className="mb-5">
                         <h3>{movie.title}</h3>
                         <span>
                           {release_date} | {original_language}
@@ -93,21 +98,28 @@ const updateColors = e => {
                             </React.Fragment>
                           ))}
                         </div>
-                      </div>
-                      <h4>Score</h4>
-                      <div className="my-2 d-flex align-items-center">
-                        <RatingCircle
-                          color="#d8454c"
-                          width={60}
-                          value={movie.vote_average * 10}
-                        />
-
-                        <div className="mx-3">{movie.vote_count} votes</div>
-                      </div>
-                      <h4>Tagline</h4>
-                      <p>{movie.tagline}</p>
-                      <h4>Overview</h4>
-                      <p>{movie.overview}</p>
+                      </section>
+                      <section className="mb-5">
+                        <h4>Overview</h4>
+                        <p>{movie.overview || "Not available"}</p>
+                      </section>
+                      {movie.tagline && (
+                        <section className="mb-4">
+                          <h4>Tagline</h4>
+                          <p>{movie.tagline || "Not available"}</p>
+                        </section>
+                      )}
+                      <section>
+                        <h4>Score</h4>
+                        <div className="my-2 d-flex align-items-center">
+                          <RatingCircle
+                            color="#d8454c"
+                            width={60}
+                            value={movie.vote_average * 10}
+                          />
+                          <div className="mx-3">{movie.vote_count} votes</div>
+                        </div>
+                      </section>
                     </div>
                   </Col>
                 </Row>
@@ -122,81 +134,3 @@ const updateColors = e => {
     </div>
   );
 };
-  };
-
-
-  export const MovieCover = ({
-    backdrop_url,
-    title,
-    release_date,
-    original_language,
-    genres
-  }) => (
-    <div
-      className="movie-cover"
-      style={{
-        backgroundImage: backdrop_url ? `url(${backdrop_url})` : "",
-        backgroundColor: !backdrop_url ? "#fff" : ""
-      }}
-    >
-      <div className="movie-cover-overlay">
-        <div className="container movie-cover-meta">
-          <div className="m-3">
-            <h3>{title}</h3>
-            <span>
-              {release_date} | {original_language}
-            </span>
-            <div>
-              {genres.map(genre => (
-                <React.Fragment key={genre.id}>
-                  <Badge variant="dark">{genre.name}</Badge>{" "}
-                </React.Fragment>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-
-export const MoviePoster = ({ poster }) => (
-  <div className="movie-poster my-2 mx-auto m-md-2">
-    <img src={poster} alt="Movie Poster" />
-  </div>
-);
-export const MovieBody = ({ movie, images }) => (
-  <Container as="main" id="movie-body-container">
-    <Row>
-      <Col>
-        <div id="overview" className="py-2">
-          <Row>
-            <Col xs={12} md={5}>
-              <MoviePoster poster={images.poster.w500}></MoviePoster>
-            </Col>
-            <Col>
-              <div className="movie-body-overview my-2 my-md-5">
-                <h4>Score</h4>
-                <div className="my-2 d-flex align-items-center justify-content-center justify-content-md-start">
-                  <RatingCircle
-                    color="#d8454c"
-                    width={60}
-                    value={movie.vote_average * 10}
-                  />
-
-                  <div className="mx-3">{movie.vote_count} votes</div>
-                </div>
-                <h4>Tagline</h4>
-                <p>{movie.tagline}</p>
-                <h4>Overview</h4>
-                <p>{movie.overview}</p>
-              </div>
-            </Col>
-          </Row>
-        </div>
-        <div id="cast"></div>
-        <div id="details"></div>
-        <div id="reviews"></div>
-      </Col>
-    </Row>
-  </Container>
-);
