e2c4a271dba4039316a91d2d41fdb7cda4de2037
zozo <zahraa.jalaa99@gmail.com>
2020-01-24 15:01:01 +0300

    fixed the utils file

--- src/index.js
+++ src/index.js
@@ -27,30 +27,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-export function disableReactDevTools(){
-  function isFunction(obj){
-    return typeof obj == "function" || false;
-  }
-  function isArray(obj) {
-    return typeof obj.forEach == "function";
-  }
-
-  function isObject(obj) {
-    var type = typeof obj;
-    return type === "function" || (type === "object" && !!obj);
-  }
-  if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
-    return;
-  }
-  
-  const NO_OP = () => {};
-  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
-      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
-    )
-      ? NO_OP
-      : isArray(window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop])
-      ? []
-      : null;
-  }
-}
--- src/utils.js
+++ src/utils.js
@@ -78,3 +78,30 @@ export class ApiError extends Error {
     this.name = "HttpError";
   }
 }
+export function disableReactDevTools(){
+  function isFunction(obj){
+    return typeof obj == "function" || false;
+  }
+  function isArray(obj) {
+    return typeof obj.forEach == "function";
+  }
+
+  function isObject(obj) {
+    var type = typeof obj;
+    return type === "function" || (type === "object" && !!obj);
+  }
+  if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+    return;
+  }
+  
+  const NO_OP = () => {};
+  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
+    )
+      ? NO_OP
+      : isArray(window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop])
+      ? []
+      : null;
+  }
+}
